a7b840acc0c5c586846ad7f4ad8f9d088d4384c8
Charan99 <111955279+CharanKanwal-99@users.noreply.github.com>
2023-11-04 20:11:00 -0600

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,32 +1,32 @@
  CREATE TABLE IF NOT EXISTS ADOPTION(
     recorded_name VARCHAR,
-    date_id INT,
-    outcome_id INT,
-    Animal_id INT,
+    date_id INT FOREIGN KEY,
+    outcome_id INT FOREIGN KEY,
+    Animal_id INT FOREIGN KEY,
     Sex VARCHAR,
-    Breed_id INT,
+    Breed_id INT FOREIGN KEY,
     Color VARCHAR,
     ID VARCHAR
     );
 
 CREATE TABLE IF NOT EXISTS OUTCOME(
     outcome VARCHAR,
-    outcome_id INT
+    outcome_id INT PRIMARY KEY
   );
 
   CREATE TABLE IF NOT EXISTS ANIMAL(
     Animal VARCHAR,
-    Animal_id INT
+    Animal_id INT PRIMARY KEY
   );
 
   CREATE TABLE IF NOT EXISTS BREED(
     Breed VARCHAR,
-    Breed_id INT
+    Breed_id INT PRIMARY KEY
   );
 
 CREATE TABLE IF NOT EXISTS DATE(
     Dt timestamp,
-    date_id INT,
+    date_id INT PRIMARY KEY,
     Mnt INT,
     Yr INT
 
